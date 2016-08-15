fb918103624d5814a81b5266832b48f7c5766ebf
David Sison <dsison3@gmail.com>
2016-08-16 00:10:15 +1000

    Fixed bug where images caused crash
    Also replaced all tabs with 4 spaces

--- PCSocBot.js
+++ PCSocBot.js
@@ -23,82 +23,84 @@ function output(error, token) {
 }
 
 client.on("message", function(message) {
-	var username = message.author.username;
-	var id = message.author.id;
-	var argv = message.content.match(/(".*?")|(\S+)/g);
-	var argc = argv.length;
-    if(argv[0] === "!tags") {
-		if (argc === 1) {
-			client.sendMessage(message, "Player tag storage for the UNSW PCSoc discord server.\n\n**`!tags`** `add` __`platform/game`__ __`tag`__\n	Adds/changes a player tag with associated platform/game to the list\n**`!tags`** `remove` __`platform/game`__\n	Removes a player tag from the list\n**`!tags`** `get` __`platform/game`__\n	Returns player tag for that discord user\n**`!tags`** __`platform/game`__\n	Displays all player tags stored for platform/game\n");
-		} else if(argv[1] === "add" && argc === 4) {
-			var app = argv[2].toLowerCase();
-			var tag = argv[3];
-			additem(id, app, tag, message, username);
-		} else if(argv[1] === "get" && argc === 3) {
-			var app = argv[2].toLowerCase();
-			getitem(id, app, message, username);
-		} else if(argv[1] === "remove" && argc === 3) {
-			var app = argv[2].toLowerCase();
-			removeitem(id, app, message, username);
-		} else if (argc === 2) {
-			var app = argv[1].toLowerCase();
-			printtags(app, message);
-		} else {
-			client.sendMessage(message, "Unknown argument(s)...");
-		}
-	}
+    var username = message.author.username;
+    var id = message.author.id;
+    var argv = message.content.match(/(".*?")|(\S+)/g);
+    if (argv !== null) {
+        var argc = argv.length;
+        if(argv[0] === "!tags") {
+            if (argc === 1) {
+                client.sendMessage(message, "Player tag storage for the UNSW PCSoc discord server.\n\n**`!tags`** `add` __`platform/game`__ __`tag`__\n    Adds/changes a player tag with associated platform/game to the list\n**`!tags`** `remove` __`platform/game`__\n    Removes a player tag from the list\n**`!tags`** `get` __`platform/game`__\n    Returns player tag for that discord user\n**`!tags`** __`platform/game`__\n    Displays all player tags stored for platform/game\n");
+            } else if(argv[1] === "add" && argc === 4) {
+                var app = argv[2].toLowerCase();
+                var tag = argv[3];
+                additem(id, app, tag, message, username);
+            } else if(argv[1] === "get" && argc === 3) {
+                var app = argv[2].toLowerCase();
+                getitem(id, app, message, username);
+            } else if(argv[1] === "remove" && argc === 3) {
+                var app = argv[2].toLowerCase();
+                removeitem(id, app, message, username);
+            } else if (argc === 2) {
+                var app = argv[1].toLowerCase();
+                printtags(app, message);
+            } else {
+                client.sendMessage(message, "Unknown argument(s)...");
+            }
+        }
+    }
 });
 
 function additem(id, app, tag, message, username) {
-	db.findOne({ _id: id }, function (err, doc) {
-		if (doc === null) {
-			var temp = { _id: id, "user": username };
-			temp[app] = tag;
-			db.insert(temp);
-		} else {
-			db.update({ _id: id }, { $set: { [app]: tag, user: username } } );
-		}
-		client.sendMessage(message, tag + " added as " + app + " tag for " + username);
-		console.log(tag + " added as " + app + " tag for " + username);
-	});
+    db.findOne({ _id: id }, function (err, doc) {
+        if (doc === null) {
+            var temp = { _id: id, "user": username };
+            temp[app] = tag;
+            db.insert(temp);
+        } else {
+            db.update({ _id: id }, { $set: { [app]: tag, user: username } } );
+        }
+        client.sendMessage(message, tag + " added as " + app + " tag for " + username);
+        console.log(tag + " added as " + app + " tag for " + username);
+    });
 }
 
 function getitem(id, app, message, username) {
-	db.findOne({ _id: id }, function (err, doc) {
-		if (doc === null || !doc.hasOwnProperty("user")) {
-			client.sendMessage(message, "User not found!");
-		} else if (!doc.hasOwnProperty(app)) {
-			client.sendMessage(message, "Platform/game not found!");
-		} else {
-			client.sendMessage(message, "The " + app + " tag of " + doc.user + " is " + doc[app]);
-		}
-	});
+    db.findOne({ _id: id }, function (err, doc) {
+        if (doc === null || !doc.hasOwnProperty("user")) {
+            client.sendMessage(message, "User not found!");
+        } else if (!doc.hasOwnProperty(app)) {
+            client.sendMessage(message, "Platform/game not found!");
+        } else {
+            client.sendMessage(message, "The " + app + " tag of " + doc.user + " is " + doc[app]);
+        }
+    });
 }
 
 function removeitem(id, app, message, username) {
-		db.findOne({ _id: id }, function (err, doc) {
-		if (doc === null) {
-			client.sendMessage(message, "User not found!");
-		} else if (!doc.hasOwnProperty(app)) {
-			client.sendMessage(message, "Platform/game not found!");
-		} else {
-			db.update({ _id: id }, { $unset: { [app]: true } });
-			client.sendMessage(message, app + " tag for " + username + " removed");
-			console.log(app + " tag for " + username + " removed");
-		}
-	});
+        db.findOne({ _id: id }, function (err, doc) {
+        if (doc === null) {
+            client.sendMessage(message, "User not found!");
+        } else if (!doc.hasOwnProperty(app)) {
+            client.sendMessage(message, "Platform/game not found!");
+        } else {
+            db.update({ _id: id }, { $unset: { [app]: true } });
+            client.sendMessage(message, app + " tag for " + username + " removed");
+            console.log(app + " tag for " + username + " removed");
+        }
+    });
 }
 
 function printtags(app, message) {
-	db.find( { [app] : { $exists: true } }, function (err, docs) {
-		if (docs.length === 0) {
-			client.sendMessage(message, "Platform/game not found!");
-		} else {
-			var mymessage = "Tags stored for " + app + ":\n";
-			for (var i = 0; i < docs.length; i++) {
-				mymessage += docs[i][app] + " [" + docs[i].user + "]\n";
-			}
-			client.sendMessage(message, mymessage);
-		}
-	});
+    db.find( { [app] : { $exists: true } }, function (err, docs) {
+        if (docs.length === 0) {
+            client.sendMessage(message, "Platform/game not found!");
+        } else {
+            var mymessage = "Tags stored for " + app + ":\n";
+            for (var i = 0; i < docs.length; i++) {
+                mymessage += docs[i][app] + " [" + docs[i].user + "]\n";
+            }
+            client.sendMessage(message, mymessage);
+        }
+    });
 }
