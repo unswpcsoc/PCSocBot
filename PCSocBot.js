536759fd8a7d564f70be59435664d1474acc1c23
Josh Wason <joshwason1@hotmail.com>
2016-11-15 15:17:14 +1100

    Update to PCSocBot.js
    Cleaned up commands using the Eris Command Framework.

--- PCSocBot.js
+++ PCSocBot.js
@@ -4,51 +4,87 @@ var Datastore = require('nedb')
 
 var request = require('request').defaults({encoding: null});
 
-var bot = new Eris("token"); //Insert Bot token here.
+
+var bot = new Eris.CommandClient("token", {}, { //Insert Bot token here.
+    description: "PC Enthusiasts Society Discod bot made with Eris",
+    owner: "David Sison, Josh Wason",
+    prefix: "~"
+});
+
 
 bot.on("ready", () => {
     console.log("Ready!");
 });
 
-bot.on("messageCreate", (message) => {
-    var username = message.author.username;
-    var id = message.author.id;
-    var argv = message.content.match;
+/* =========================COMMANDS========================= */
 
-    if(message.content === "!ping") {
-        bot.createMessage(message.channel.id, "Pong!");
-    }
-
-    if(message.content === "!meme") {
-        request.get("http://puu.sh/s0lxS.gif", function(err, res, buffer) { //URL is a placeholder GIF
-            bot.createMessage(channelID, message.author.mention, {name: 'meme.gif', file: buffer}); //Replace channelID with the ID of the channel that you want the image to be posted in. To send in same channel as the command was used in use message.channel.id
-        });
-    }
-
-    if (argv !== null) {
-        var argc = argv.length;
-        if(argv[0] === "!tags") {
-            if (argc === 1) {
-                bot.createMessage(message.channel.id, "Player tag storage for the UNSW PCSoc discord server.\n\n**`!tags`** `add` __`platform/game`__ __`tag`__\n    Adds/changes a player tag with associated platform/game to the list\n**`!tags`** `remove` __`platform/game`__\n    Removes a player tag from the list\n**`!tags`** `get` __`platform/game`__\n    Returns player tag for that discord user\n**`!tags`** __`platform/game`__\n    Displays all player tags stored for platform/game\n");
-            } else if(argv[1] === "add" && argc === 4) {
-                var app = argv[2].toLowerCase();
-                var tag = argv[3];
-                additem(id, app, tag, message, username);
-            } else if(argv[1] === "get" && argc === 3) {
-                var app = argv[2].toLowerCase();
-                getitem(id, app, message, username);
-            } else if(argv[1] === "remove" && argc === 3) {
-                var app = argv[2].toLowerCase();
-                removeitem(id, app, message, username);
-            } else if (argc === 2) {
-                var app = argv[1].toLowerCase();
-                printtags(app, message);
-            } else {
-                bot.createMessage(message.channel.id, "Unknown argument(s)...");
-            }
-        }
-    }
+bot.registerCommand("ping", "Pong!", {
+    description: "Pong!",
+    fullDescription: "This command could be used to check if the bot is up. Or entertainment when you're bored.",
+    caseInsensitive: true
 });
+
+bot.registerCommand("pong", "Ping!", {
+    description: "Ping!",
+    fullDescription: "This command could be used to check if the bot is up. Or entertainment when you're bored.",
+    caseInsensitive: true
+});
+
+var tags_CMD = bot.registerCommand("tags", "Player tag storage for the UNSW PCSoc discord server.\n\n**`!tags`** `add` __`platform/game`__ __`tag`__\n    Adds/changes a player tag with associated platform/game to the list\n**`!tags`** `remove` __`platform/game`__\n    Removes a player tag from the list\n**`!tags`** `get` __`platform/game`__\n    Returns player tag for that discord user\n**`!tags`** __`platform/game`__\n    Displays all player tags stored for platform/game\n", {
+    description: "Player tag storage for the UNSW PCSoc discord server.",
+    fullDescription: "This command stores user/player tags for any platform. Can be used to search up your own tags and other users/players on the server.",
+    caseInsensitive: true
+});
+
+tags_CMD.registerSubcommand("add", (msg, args) => {
+    if(args.length !== 2) {
+        return "Invalid input";
+    } else {
+        additem(msg.member.id, args[0].toLowerCase(), args[1], msg, msg.member.user.username);
+    }
+}, {
+    description: "Add tags.",
+    fullDescription: "Adds a user/player tag to the bot.",
+    usage: "<platform/game> <tag>"
+});
+
+tags_CMD.registerSubcommand("get", (msg, args) => {
+    if(args.length !== 1) {
+        return "Invalid input";
+    } else {
+        getitem(msg.member.id, args[0].toLowerCase(), msg, msg.member.user.username);
+    }
+}, {
+    description: "Get tags.",
+    fullDescription: "Returns your own tag for a platform/game",
+    usage: "<platform/game>"
+});
+
+tags_CMD.registerSubcommand("remove", (msg, args) => {
+    if(args.length !== 1) {
+        return "Invalid input";
+    } else {
+        removeitem(msg.member.id, args[0].toLowerCase(), msg, msg.member.user.username);
+    }
+}, {
+    description: "Removes tags.",
+    fullDescription: "Removes a user/player tag to the bot.",
+    usage: "<platform/game>"
+});
+
+tags_CMD.registerSubcommand("list", (msg, args) => {
+    if(args.length !== 1) {
+        return "Invalid input";
+    } else {
+        printtags(args[0].toLowerCase(), msg);
+    }
+}, {
+    description: "List tags.",
+    fullDescription: "Returns a list of user tags for a specified platform.",
+    usage: "<platform/game>"
+});
+
+/* ========================================================== */
 
 var t = setInterval(highNoon, 1000);
 
@@ -58,15 +94,15 @@ function highNoon() {
     let m = date.getMinutes();
     let s = date.getSeconds();
 
-    if (h === 1 && m === 00 && s === 0) {
+    if (h === 12 && m === 0 && s === 0) {
         request.get("http://vignette3.wikia.nocookie.net/overwatch/images/f/f3/Mccree_portrait.png", function(err, res, buffer) {
-            bot.createMessage(channelID, "It's high noon", {name: 'mccree.png', file: buffer}); //Replace channelID with the ID of the text channel that you wish to use this function.
+            bot.createMessage(chanelID, "It's high noon", {name: 'mccree.png', file: buffer}); //Replace channelID with the ID of the text channel that you wish to use this function.
         });
     }
 }
 
 
-function additem(id, app, tag, message, username) {
+function additem(id, app, tag, msg, username) {
     db.findOne({ _id: id }, function (err, doc) {
         if (doc === null) {
             var temp = { _id: id, "user": username };
@@ -75,47 +111,47 @@ function additem(id, app, tag, message, username) {
         } else {
             db.update({ _id: id }, { $set: { [app]: tag, user: username } } );
         }
-        bot.createMessage(tag + " added as " + app + " tag for " + username);
+        bot.createMessage(msg.channel.id, tag + " added as " + app + " tag for " + username);
         console.log(tag + " added as " + app + " tag for " + username);
     });
 }
 
-function getitem(id, app, message, username) {
+function getitem(id, app, msg, username) {
     db.findOne({ _id: id }, function (err, doc) {
         if (doc === null || !doc.hasOwnProperty("user")) {
-            bot.createMessage(message.channel.id, "User not found!");
+            bot.createMessage(msg.channel.id, "User not found!");
         } else if (!doc.hasOwnProperty(app)) {
-            bot.createMessage(message.channel.id, "Platform/game not found!");
+            bot.createMessage(msg.channel.id, "Platform/game not found!");
         } else {
-            bot.createMessage(message.channel.id, "The " + app + " tag of " + doc.user + " is " + doc[app]);
+            bot.createMessage(msg.channel.id, "The " + app + " tag of " + doc.user + " is " + doc[app]);
         }
     });
 }
 
-function removeitem(id, app, message, username) {
+function removeitem(id, app, msg, username) {
         db.findOne({ _id: id }, function (err, doc) {
         if (doc === null) {
-            bot.createMessage(message.channel.id, "User not found!");
+            bot.createMessage(msg.channel.id, "User not found!");
         } else if (!doc.hasOwnProperty(app)) {
-            bot.createMessage(message.channel.id, "Platform/game not found!");
+            bot.createMessage(msg.channel.id, "Platform/game not found!");
         } else {
             db.update({ _id: id }, { $unset: { [app]: true } });
-            bot.createMessage(message.channel.id, app + " tag for " + username + " removed");
+            bot.createMessage(msg.channel.id, app + " tag for " + username + " removed");
             console.log(app + " tag for " + username + " removed");
         }
     });
 }
 
-function printtags(app, message) {
+function printtags(app, msg) {
     db.find( { [app] : { $exists: true } }, function (err, docs) {
         if (docs.length === 0) {
-            bot.createMessage(message.channel.id, "Platform/game not found!");
+            bot.createMessage(msg.channel.id, "Platform/game not found!");
         } else {
             var mymessage = "Tags stored for " + app + ":\n";
             for (var i = 0; i < docs.length; i++) {
                 mymessage += docs[i][app] + " [" + docs[i].user + "]\n";
             }
-            bot.createMessage(message.channel.id, mymessage);
+            bot.createMessage(msg.channel.id, mymessage);
         }
     });
 }
