e4d8c9f9801e83e0d0186a28e364e162792373a3
VineBalloon <vincentchen2.033@gmail.com>
2018-06-30 21:46:56 +1000

    Added ponyORM to the install script

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@
 # this stuff is required for everything
 sudo pip3 install discord.py
 
+# this stuff is required for !tags
+sudo pip3 install pony
+
 # this stuff is only required for voice channels
 sudo apt install python3-dev libffi-dev ffmpeg
 sudo pip3 install PyNaCL mutagen
