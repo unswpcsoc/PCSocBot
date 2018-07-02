a9f5f34c094d074710cae29f973a7416a8f55def
VineBalloon <vincentchen2.033@gmail.com>
2018-07-02 12:42:26 +1000

    Cleaned up someone.py and added extra functionality to Remove

--- commands/someone.py
+++ commands/someone.py
@@ -92,35 +92,44 @@ class Add(Someone):
         return "Your format %s for %s people has been added!" % (code(format_string), code(str(people)))
 
 class Remove(Someone):
-    desc = "Removes a format template. Mods only."
+    desc = """Removes a format template. 
+    Passing a number removes all formats for those people. Mods only."""
 
     #roles_required = ['mod', 'exec']
     def eval(self, *format_string):
 
+        # If a number is passed into the second argument, set remove_all flag
+        try:
+            people = int(format_string)
+            remove_all = True
+        except ValueError:
+            pass
+
         # Get the format string
         format_string = " ".join(format_string)
 
-        # Count the number of people to generate
+        # Get people
         people = format_string.count(IDENTIFIER)
 
-        # Open the JSON file or create a new dict to load
         try:
-            with open(FORMAT_FILE, 'r') as old:
-                formats = json.load(old)
-        except FileNotFoundError:
-            formats = {}
+            # Open the JSON file or create a new dict to load
+            with open(FORMAT_FILE, 'r') as fmt:
+                formats = json.load(fmt)
 
-        # Check if format string is in the dict
-        try:
-            formats[str(people)].remove(format_string)
-        except KeyError:
-            pass
+            # Remove format string if in the dict
+            if remove_all:
+                del formats[str(people)]
+            else:
+                formats[str(people)].remove(format_string)
+
+        except (FileNotFoundError, KeyError):
+            return "Format %s not found!" % code(format_string)
 
         # Write the formats to the JSON file
         with open(FORMAT_FILE, 'w') as new:
             json.dump(formats, new)
 
-        return "If format %s for %s people existed, it was removed!" % (code(format_string), code(str(people)))
+        return "If the format %s for %s people existed, it was removed!" % (code(format_string), code(str(people)))
 
 class List(Someone):
     desc = "Lists the formats for the given number of `people`."
