d3ee34dbc5fd2377ed17b31598bbfc63ecac1047
Vincent Chen <vincentchen2.033@gmail.com>
2018-07-18 22:39:31 +1000

    !mod (#21)
    * Hotfix for !someone list. Messages over the limit are now split into multiple messages
    
    * Uncommented the twitch feature
    
    * Added !mod; ping a random mod
    
    * Added !mod; ping a random mod

---
+++ commands/mod.py
@@ -0,0 +1,9 @@
+from commands.base import Command
+from helpers import *
+
+import random
+
+class Mod(Command):
+    desc = "This command is used to mention a random mod in times of need. Not a smart idea to abuse this command"
+
+    def eval(self): return "Paging %s..." % random.choice([ x for x in list(self.members) if "mod" in [ str(y).lower() for y in x.roles ] ]).mention
