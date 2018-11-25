f4622c4862bb17b36ec409f927b6a3dec1eca1ea
Terry Agapitos <terry@agapitos.com.au>
2018-11-25 20:48:30 +1100

    Pig Latin bug fixes (#40)
    * Fixed piglatin to enforce args
    
    * Bot can't call the function (prevents recursive calling), only removes first letter on words that have an alphanumeric non-vowel letter.
    
    * cleanup
    
    * This check is now done for every message - not just for piglatin commands (PR #41)

--- commands/piglatin.py
+++ commands/piglatin.py
@@ -1,8 +1,21 @@
 from commands.base import Command
-# from helpers import *
 
 class PigLatin(Command):
     desc = "This command is used to translate a given sentence into Pig Latin."
 
     def eval(self, *args):
-        return ' '.join([arg + 'ay' if arg[0].lower() in 'aeiou' else arg[1:] + arg[0] + 'ay' for arg in args])
+        suffix = 'ay'
+        res = []
+        if args:
+            # There are arguments and it's not PCSocBot conducting the command
+            for arg in args:
+                if arg[0].lower() in 'aeiou':
+                    # Starts with a vowel
+                    res.append(arg + suffix)
+                elif arg[0].isalpha():
+                    # Consonant
+                    res.append(arg[1:] + arg[0] + suffix)
+                else:
+                    # Anything else (eg emotes, mentions)
+                    res.append(arg)
+        return ' '.join(res) if res else None
