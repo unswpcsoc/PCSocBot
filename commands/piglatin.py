c909c5d632ece11a6e43b8cc8835dfe971fa0c38
Terry Agapitos <terry@agapitos.com.au>
2018-11-25 17:59:07 +1100

    Fixed piglatin to enforce args

--- commands/piglatin.py
+++ commands/piglatin.py
@@ -5,4 +5,5 @@ class PigLatin(Command):
     desc = "This command is used to translate a given sentence into Pig Latin."
 
     def eval(self, *args):
-        return ' '.join([arg + 'ay' if arg[0].lower() in 'aeiou' else arg[1:] + arg[0] + 'ay' for arg in args])
+        if args:
+            return ' '.join([arg + 'ay' if arg[0].lower() in 'aeiou' else arg[1:] + arg[0] + 'ay' for arg in args])
