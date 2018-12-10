7c82ae763a48b5ba9a4f08dca545ae693d333c45
Terry Agapitos <terry@agapitos.com.au>
2018-12-10 22:34:43 +1100

    Refactor of archive.py

--- commands/archive.py
+++ commands/archive.py
@@ -9,10 +9,11 @@ from helpers import *
 HISTORY = 10
 HISTORY_LIMIT = 500
 SCROLL_UTF = "\U0001F4DC"
-HOTLINK_PREFIX = "https://discordapp.com/channels/"
-ARCHIVE_CHANNEL = "463602892717162497"
+HOTLINK_PREFIX = "https://discordapp.com/channels"
+ARCHIVE_CHANNEL = "474009272804442112"
 AVATAR_FORMAT = "https://cdn.discordapp.com/avatars/{0.id}/{0.avatar}.png?size=128"
 IMAGE_EXTENSIONS = ['png', 'jpg', 'jpeg', 'gif']
+
 
 class Archive(Command):
     desc = "Archive command to store the best of PCSoc. Mods only."
@@ -20,63 +21,77 @@ class Archive(Command):
 
     async def eval(self, index):
         try:
-            if int(index) >= HISTORY:
+            int_index = int(index)
+            if int_index < 0:
+                raise ValueError
+
+            if int_index >= HISTORY:
                 message = await self.client.get_message(self.message.channel, index)
                 entry = Entry(index, message)
             else:
-                archive = await create_archive(self.client.logs_from(self.message.channel,
-                                                                     limit=HISTORY_LIMIT))
-                entry = archive[int(index)]
-            channel = self.client.get_channel(ARCHIVE_CHANNEL)
-            header = entry.hotlink
-            footer = "Archived message from #{}".format(self.message.channel)
-            await self.client.send_message(channel, header, embed=entry.as_embed(footer))
-            return "Archived message {} in <#{}>".format(index, ARCHIVE_CHANNEL)
+                archive = await create_archive(
+                    self.client.logs_from(
+                        self.message.channel, limit=HISTORY_LIMIT))
+                if not archive or int_index >= len(archive):
+                    return f"Can't find message {bold(index)}!"
+                entry = archive[int_index]
+        except ValueError:
+            return "Please input a non-negative number!"
+        except NotFound:
+            return f"Message ID {bold(index)} doesn't exist!"
+        except (Forbidden, HTTPException):
+            return f"Could not archive message {bold(index)}!"
 
-        except (IndexError, ValueError, NotFound, Forbidden, HTTPException):
-            return "Could not archive message %s!" % bold(index)
+        channel = self.client.get_channel(ARCHIVE_CHANNEL)
+        if channel is None:
+            raise CommandFailure("Can't find archive channel!")
+
+        footer = f"Archived message from #{self.message.channel}"
+        await self.client.send_message(channel, entry.hotlink, embed=entry.as_embed(footer))
+        return f"Archived message {index} in <#{ARCHIVE_CHANNEL}>"
+
 
 class List(Archive):
     desc = "Lists recent messages available for archiving. Mods only."
+
     async def eval(self):
-        archive = await create_archive(self.client.logs_from(self.message.channel,
-                                                         limit=HISTORY_LIMIT))
+        archive = await create_archive(
+            self.client.logs_from(self.message.channel,
+                                  limit=HISTORY_LIMIT))
         out = [entry.as_text() for entry in archive]
         if out:
-            return SCROLL_UTF + "Last %d Archiveable Messages:\n" % len(out) + "\n".join(out)
-        return "No messages available for archival. React with a " + SCROLL_UTF \
-            + " to a message in the last " + str(HISTORY_LIMIT) + " messages of"\
-            + " this channel to mark it for archival."
+            return f"{SCROLL_UTF} Last {len(out)} Archiveable Messages:\n" + "\n".join(out)
+        return f"No messages available for archival. React with a {SCROLL_UTF} " \
+            f"to a message in the last {HISTORY_LIMIT} messages of " \
+            f"this channel to mark it for archival."
+
 
 class Ls(Archive):
-    desc = "See " + bold(code("!archive") + " " + code("list")) + "."
+    desc = f"See {bold(code('!archive'))} {bold(code('list'))}."
+
     async def eval(self):
         return await List.eval(self)
+
 
 class Entry():
     def __init__(self, index, message):
         self.index = index
-        reactions = [x.emoji for x in message.reactions]
-        try:
-            self.reactions = message.reactions[reactions.index(SCROLL_UTF)].count
-        except ValueError:
-            self.reactions = 0
+        self.reactions = 0
+        # Get number of scroll reacts
+        for reaction in message.reactions:
+            if reaction.emoji == SCROLL_UTF:
+                self.reactions = reaction.count
+                break
+
         self.author = message.author
         self.content = message.clean_content
         self.attachments = message.attachments
         self.timestamp = message.timestamp
-        self.hotlink = "{}{}/{}/{}".format(HOTLINK_PREFIX,
-            message.server.id, message.channel.id, message.id)
+        self.hotlink = f"{HOTLINK_PREFIX}/{message.server.id}/{message.channel.id}/{message.id}"
 
     def as_text(self):
-        # Show index
-        text = str(self.index) + ". "
-
-        # Show reaction count
-        text += "[%s%d]  " % (SCROLL_UTF, self.reactions)
-
-        # Show author
-        text += nick(self.author)
+        # Show index, reaction count, author
+        text = f"{self.index}. [{SCROLL_UTF}{self.reactions}]  {nick(self.author)}"
 
         # Bold entire first line
         text = bold(text)
@@ -105,23 +120,25 @@ class Entry():
 
     def as_embed(self, title):
         embed = Embed(description=self.content,
-                     colour=self.author.colour,
-                     timestamp=self.timestamp)
+                      colour=self.author.colour,
+                      timestamp=self.timestamp)
         embed.set_author(name=nick(self.author),
                          icon_url=AVATAR_FORMAT.format(self.author))
         embed.set_footer(text=title)
         attached_image = False
-        for a in self.attachments:
-            if is_image(a["url"]):
-                embed.set_image(url=a["url"])
+        for attachment in self.attachments:
+            if is_image(attachment["url"]):
+                embed.set_image(url=attachment["url"])
                 attached_image = True
             else:
-                embed.add_field(name="Attachment", value=a["url"], inline=False)
+                embed.add_field(name="Attachment",
+                                value=attachment["url"], inline=False)
         if not attached_image:
             if is_image(self.content):
                 embed.set_image(url=self.content)
-            
+
         return embed
+
 
 async def create_archive(logs):
     i = 0
@@ -133,17 +150,16 @@ async def create_archive(logs):
         if i >= HISTORY:
             break
 
-        reactions = [x.emoji for x in message.reactions]
         # Find those messages that have scroll emoji reactions
-        if SCROLL_UTF in reactions:
-
+        if any(SCROLL_UTF == x.emoji for x in message.reactions):
             # Append entry to archive
             archive.append(Entry(i, message))
 
             # Increment history counter
             i += 1
-    
+
     return archive
+
 
 def is_image(url):
     return url.split('.')[-1].lower() in IMAGE_EXTENSIONS
