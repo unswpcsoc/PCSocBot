e48ddff932e52cc3e118f0d34279860fab8168e9
Terry Agapitos <terry@agapitos.com.au>
2018-12-11 00:44:02 +1100

    forgot one

--- commands/roles.py
+++ commands/roles.py
@@ -4,21 +4,21 @@ from helpers import *
 
 # class Weeb(Command):
 #     desc = "Gives/removes the Weeb role."
-    
+
 #     async def eval(self):
 #         return await assign_role(self.client, self.message, "Weeb")
 
 
 class Wiki(Command):
     desc = "Gives/removes the Wiki Gremlin role."
-    
+
     async def eval(self):
         return await assign_role(self.client, self.message, "Wiki Gremlin")
 
 
 class Meta(Command):
     desc = "Gives/removes the Meta role."
-    
+
     async def eval(self):
         return await assign_role(self.client, self.message, "Meta")
 
@@ -35,7 +35,7 @@ async def assign_role(client, message, role_name):
     await client.add_roles(message.author, role)
     return message.author.mention + " is now a " + role_name
 
-                
+
 def find_role(roles, role_name):
     for role in roles:
         if role_name == role.name:
