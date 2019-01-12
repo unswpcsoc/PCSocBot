b76aeadd54b2cba7b7b8e30f7597e34d253e6594
VineBalloon <vincentchen2.033@gmail.com>
2019-01-12 16:28:08 +1100

    go away

--- commands/branch.py
+++ commands/branch.py
@@ -3,11 +3,11 @@ from helpers import code
 
 import re, subprocess
 
-BRANCH = r'git rev-parse --abbrev-ref HEAD'
+BRANCH = code(subprocess.getoutput(r'git rev-parse --abbrev-ref HEAD'))
 
 
 class Branch(Command):
     desc = 'Returns what feature branch PCSocBot is currently running on.'
 
     def eval(self):
-        return "Running on " + code(subprocess.getoutput(BRANCH))
+        return "Running on " + BRANCH
