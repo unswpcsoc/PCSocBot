d17aa243bb3c48f9967cbc288ec2dc18f8f011a0
Vincent Chen <vincentchen2.033@gmail.com>
2019-01-12 16:32:43 +1100

    Fixed branch not working, thanks Harrison (#56)
    * Fixed branch not working, thanks Harrison
    
    * go away

--- commands/branch.py
+++ commands/branch.py
@@ -3,11 +3,11 @@ from helpers import code
 
 import re, subprocess
 
+BRANCH = code(subprocess.getoutput(r'git rev-parse --abbrev-ref HEAD'))
+
 
 class Branch(Command):
     desc = 'Returns what feature branch PCSocBot is currently running on.'
 
     def eval(self):
-        output = subprocess.getoutput("git branch")
-        branch = re.search('\n\* (.*?)\n', output).group(1)
-        return "Running on " + code(branch)
+        return "Running on " + BRANCH
