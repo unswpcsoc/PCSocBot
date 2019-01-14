7262736f0aab95e640c6fad0bb1fbb308b4ed816
VineBalloon <vincentchen2.033@gmail.com>
2019-01-14 12:00:07 +1100

    Merge master into mfast

--- commands/handbook.py
+++ commands/handbook.py
@@ -1,10 +1,6 @@
 from commands.base import Command
 from discord import Embed
-<<<<<<< HEAD
-from helpers import CommandFailure, is_good_response
-=======
 from helpers import bold, CommandFailure, is_good_response
->>>>>>> master
 
 import bs4, re, requests
 
@@ -52,24 +48,6 @@ def subject_details(code):
 
 
 class Handbook(Command):
-<<<<<<< HEAD
-    desc = "This command scrapes entries in the UNSW handbook"
-
-    def eval(self, course_code):
-
-        if not re.search(r'^[a-zA-Z]{4}[0-9]{4}$', course_code):
-            raise CommandFailure('Incorrectly formatted course code: ' + course_code)
-
-        course = subject_details(arg)
-        if course is None:
-            return 'Course ' + arg + ' could not be found'
-
-        ret = Embed(title=course['title'], description=course['description'], url=course['link'], color=self.EMBED_COLOR)
-        ret.add_field(name='Offering Terms', value=course['offerings'])
-        ret.add_field(name='Enrolment Conditions', value=course['conditions'])
-
-        return ret
-=======
     desc = "This command scrapes entries in the UNSW handbook."
 
     def eval(self, course_code):
@@ -89,4 +67,3 @@ class Handbook(Command):
         ret.add_field(name='Offering Terms', value=course['offerings'])
         ret.add_field(name='Enrolment Conditions', value=course['conditions'])
         return ret
->>>>>>> master
