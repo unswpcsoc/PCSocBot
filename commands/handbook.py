078aeb85b30c165e19bc67479882fca375a93c36
VineBalloon <vincentchen2.033@gmail.com>
2019-01-14 11:33:42 +1100

    Fixed according to review

--- commands/handbook.py
+++ commands/handbook.py
@@ -1,13 +1,12 @@
 from commands.base import Command
 from discord import Embed
-from helpers import CommandFailure, is_good_response
+from helpers import bold, CommandFailure, is_good_response
 
 import bs4, re, requests
 
 
 def subject_details(code):
     code = code.upper()
-
     page = requests.get('https://www.handbook.unsw.edu.au/undergraduate/courses/2019/' + code)
 
     if not is_good_response(page):
@@ -17,36 +16,29 @@ def subject_details(code):
             return None
 
     soup = bs4.BeautifulSoup(page.text, features='lxml')
-
     course_title = soup.find('span', attrs={'data-hbui' : 'module-title'}).string.strip()
     course_offerings = soup.find('strong', string=re.compile('Offering Terms')).parent.contents[3].string.strip()
-
     course_conditions_tag = soup.find('div', id='readMoreSubjectConditions')
 
     course_conditions = ''
     if course_conditions_tag is not None:
         for s in course_conditions_tag.contents[1].contents[1].strings:
             course_conditions += s
+        course_conditions = course_conditions.strip()
     else:
         course_conditions = 'None'
 
-    course_conditions = course_conditions.strip()
-
-    course_description_tag = soup.find('div', id='readMoreIntro')
-
-    course_description = ''
-
-    if len(course_description_tag.contents[1].contents) > 1 and course_description_tag.contents[1].contents[1].name == 'p':
-        for s in course_description_tag.contents[1].contents[1].strings:
-            course_description += s
+    course_desc = ''
+    course_desc_cont = soup.find('div', id='readMoreIntro').contents[1].contents
+    if len(course_desc_cont) > 1 and course_desc_cont[1].name == 'p':
+        for s in course_desc_cont[1].strings:
+            course_desc += s
     else:
-        course_description = course_description_tag.contents[1].contents[0].string
-
-    course_description = course_description.strip()
-
+        course_desc = course_desc_cont[1].contents[0].string
+    course_desc = course_desc.strip()
     return {
         'title' : course_title,
-        'description' : course_description,
+        'description' : course_desc,
         'offerings' : course_offerings,
         'conditions' : course_conditions,
         'link' : 'https://www.handbook.unsw.edu.au/undergraduate/courses/2019/' + code
@@ -54,19 +46,22 @@ def subject_details(code):
 
 
 class Handbook(Command):
-    desc = "This command scrapes entries in the UNSW handbook"
+    desc = "This command scrapes entries in the UNSW handbook."
 
     def eval(self, course_code):
-
-        if not re.search(r'^[a-zA-Z]{4}[0-9]{4}$', course_code):
-            raise CommandFailure('Incorrectly formatted course code: ' + course_code)
+        if re.search(r'^[a-zA-Z]{4}[0-9]{4}$', course_code) is None:
+            raise CommandFailure('Incorrectly formatted course code: ' + bold(course_code))
 
         course = subject_details(course_code)
         if course is None:
             return 'Course ' + course_code + ' could not be found'
 
-        ret = Embed(title=course['title'], description=course['description'], url=course['link'], color=self.EMBED_COLOR)
+        ret = Embed(
+                title=course['title'], 
+                description=course['description'], 
+                url=course['link'], 
+                color=self.EMBED_COLOR
+                )
         ret.add_field(name='Offering Terms', value=course['offerings'])
         ret.add_field(name='Enrolment Conditions', value=course['conditions'])
-
         return ret
