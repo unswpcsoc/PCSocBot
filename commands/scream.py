8a3e2e423400a03bcf309f80fbe546d92b02fa73
VineBalloon <vincentchen2.033@gmail.com>
2019-03-30 23:17:47 +1100

    scream

---
+++ commands/scream.py
@@ -0,0 +1,40 @@
+from commands.base import Command
+
+from random import choice, randrange
+
+
+class Scream(Command):
+    desc = "fnshkdhnjksdg"
+
+    def eval(self):
+        # rare change of HHHhhhhh
+        if randrange(0, 100) == 1:
+            return 'H' * randrange(0, 5) + 'h' * randrange(1, 10)
+
+        # rare chance of waaaAAAA, thanks tiff
+        if randrange(0, 100) == 1:
+            return 'w' + 'a' * randrange(0, 5) + 'A' * randrange(1, 10)
+
+        # regular homerow mashing
+        # tends to use home row + b and n
+        screams = list("asdfghjklbn")
+        # screaming likely begins at the index/middle finger except for d
+        starts = list("fjk")
+
+        if randrange(0, 2) == 1:
+            screams = list(map(str.upper, screams))
+            starts = list(map(str.upper, starts))
+        else:
+            screams = list(map(str.lower, screams))
+            starts = list(map(str.lower, starts))
+        out = choice(starts)
+
+        for _ in range(randrange(5, 15)):
+            c = choice(screams)
+            # make sure we don't repeat a char unless it's a h
+            if c != 'h' and c != 'H':
+                while c == out[-1]: 
+                    c = choice(screams)
+            out += c
+
+        return out
