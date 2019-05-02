f063b8c613cba7f1bfc42822f0cee3f6f0554e3c
VineBalloon <vincentchen2.033@gmail.com>
2019-05-02 23:38:27 +1000

    might work iunno

--- main.go
+++ main.go
@@ -23,47 +23,103 @@ type leaf struct {
 	leaves  map[string]*leaf
 }
 
+// NewLeaf Returns a leaf with the command
+func NewLeaf(cm *com.Command) *leaf {
+	return &leaf{
+		command: cm,
+		leaves:  make(map[string]*leaf),
+	}
+}
+
 // Router Routes a command string to a command
 type Router struct {
 	routes *leaf
 }
 
 // AddCommand Adds command-string mapping
-func (r *Router) AddCommand(command com.Command, argv []string) {
-	// TODO: Tree
-	// AddCommand(PingTagPeople, ask)
-	// AddCommand(PingTagPeople, tags, ping)
+func (r *Router) AddCommand(cm *com.Command, cmdstr []string) {
+	// AddCommand(PingTagPeople, {"ask"})
+	// AddCommand(PingTagPeople, {"tags", "ping"})
+	if cm == nil || len(cmdstr) == 0 {
+		return
+	}
+	if r.routes != nil {
+		// Search all known leaves
+		curr := r.routes
+		for {
+			curr, found = curr.leaves[cmdstr[0]]
+			if !found {
+				break
+			}
+			cmdstr = cmdstr[1:]
+		}
+	}
+	// Add new leaves
+	for len(cmdstr) > 0 {
+		curr.leaves[cmdstr[0]] = NewLeaf(nil)
+		curr = curr.leaves[cmdstr[0]]
+		cmdstr = cmdstr[1:]
+	}
+	// Assign command to the final leaf
+	curr.command = cm
 }
 
-// Route Routes to handler from string
-func (r *Router) Route(argv []string) com.Command {
+/*
+ * Route Routes to handler from string.
+ * Returns the command and an empty slice if found.
+ * Returns nil and the slice of matched words to use in command assistance.
+ */
+func (r *Router) Route(argv []string) (*com.Command, []string) {
 	if r.routes == nil {
-		return nil
+		return nil, false
 	}
 	if len(argv) == 0 {
-		return nil
+		return nil, false
 	}
-	return doRoute(r.routes, argv[1:])
+	curr := r.routes
+	for len(argv) > 0 {
+		curr, found := curr[argv[0]]
+		if !found {
+			break
+		}
+		argv = argv[1:]
+	}
+	/*
+		cm, _ := doRoute(r.routes, argv[1:])
+		if cm == nil {
+			return nil, false
+		}
+	*/
 }
 
-func doRoute(leaf *leaf, argv []string) com.Command {
-	if leaf == nil {
-		return nil
+/*
+// doRoute Recursive helper for Route
+func doRoute(lf *leaf, argv []string) (*com.Command, []string) {
+	if lf == nil {
+		return nil, nil
 	}
-	val, found := leaf.leaves[argv[0]]
+	val, found := lf.leaves[argv[0]]
 	if found {
-		if len(argv) == 0 {
-			return val.command
+		ret, args := doRoute(val, argv[1:])
+		if ret == nil {
+			// Next node is end, use current lf and argv
+			return lf.command, argv[1:]
+		} else {
+			// Next node isn't end, pass up
+			return ret, args
 		}
-		return doRoute(val, argv[1:])
+	} else {
+		// End search, pass up argv
+		return nil, argv
 	}
 }
+*/
 
 // init for discordgo things
 func init() {
 	token, exists := os.LookupEnv("TOKEN")
 	if !exists {
-		log.Fatal("Missing Discord API Key: TOKEN")
+		log.Fatal("Missing Discord API Key: Set env var $TOKEN")
 	}
 
 	var err error
@@ -81,7 +137,10 @@ func init() {
 
 // command initialisation
 func init() {
-
+	ping := com.NewPing()
+	for _, names := range ping.Names() {
+		router.AddCommand(ping, strings.Split(names, " "))
+	}
 }
 
 func main() {
