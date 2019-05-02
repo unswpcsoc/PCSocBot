03b560d40dbc145156478f11ed637f50ba6784b8
VineBalloon <vincentchen2.033@gmail.com>
2019-05-03 01:56:20 +1000

    Added more methods for CommandSend

--- commands/commands.go
+++ commands/commands.go
@@ -2,6 +2,8 @@ package commands
 
 import (
 	"github.com/bwmarrin/discordgo"
+
+	"github.com/unswpcsoc/PCSocBot/utils"
 )
 
 // Command The command
@@ -11,19 +13,72 @@ type Command interface {
 	Roles() []string
 	Channels() []string
 
-	MsgHandle() (Send, error)
+	MsgHandle(*discordgo.Session, *discordgo.MessageCreate) (Send, error)
 }
 
 // Send Stores the stuff we need to send
-type Send struct {
-	BeegYoshi map[int]discordgo.MessageSend
+type CommandSend struct {
+	data      []*discordgo.MessageSend
+	channelid string
 }
 
-func (s *Send) AddChungus() {
+// NewSend Returns a send struct.
+func NewSend(cid string) *CommandSend {
+	return &CommandSend{
+		make([]*discordgo.MessageSend),
+		cid,
+	}
+}
+
+// NewSimpleSend Returns a send struct with the message content filled in.
+func NewSimpleSend(cid string, msg string) *CommandSend {
+	send := discordgo.MessageSend{
+		Content: msg,
+		nil,
+		nil,
+		nil,
+		nil,
+	}
+	return &CommandSend{
+		data:      []*discordgo.MessageSend{send},
+		channelid: cid,
+	}
+}
+
+// AddSimpleMessage Adds another simple message to be sent.
+func (c *CommandSend) AddSimpleMessage(msg string) {
+	send := discordgo.MessageSend{
+		Content: msg,
+		nil,
+		nil,
+		nil,
+		nil,
+	}
+	append(c.data, send)
+}
+
+// AddEmbedMessage Adds an embed message to be sent.
+func (c *CommandSend) AddEmbedMessage(emb *discordgo.MessageEmbed) {
+	send := discordgo.MessageSend{
+		nil,
+		emb,
+		nil,
+		nil,
+		nil,
+	}
+	append(c.data, send)
+}
+
+// AddMessageSend Adds a pure message embed to be sent.
+func (c *CommandSend) AddMessageSend(msd *discordgo.MessageSend) {
+	append(c.data, send)
+}
+
+// Send Sends the messages a command returns while also checking message length
+func (c *CommandSend) Send(s *discordgo.Session) {
 	// Get the stuff out of BeegYoshi and send it into the server
-
-	...
-
-	// Other stuff to do (eg: add reacts probably done in Main)
-	// ()
+	// TODO: Check message length
+	for _, data := range c.data {
+		s.ChannelMessageSendComplex(c.channelid, data)
+	}
 }
