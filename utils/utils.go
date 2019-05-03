405aca1eae30afdab7dd9f2a06612aa4b528e7ad
VineBalloon <vincentchen2.033@gmail.com>
2019-05-03 14:27:40 +1000

    Recursive string length counter tested and working

--- utils/utils.go
+++ utils/utils.go
@@ -2,8 +2,6 @@ package utils
 
 import (
 	"reflect"
-
-	"github.com/bwmarrin/discordgo"
 )
 
 func Italics(s string) string {
@@ -23,28 +21,53 @@ func Spoil(s string) string {
 }
 
 // Strlen Recursively searches for strings and counts up the total length
-func Strlen(e *interface{}) int {
+func Strlen(e interface{}) int {
 	count := 0
-	v := reflect.ValueOf(e)
-	for i := range v.NumField() {
-		// iterate over fields
-		switch f := v.Field(i); f.Kind() {
-		case reflect.String:
-			// count string field lengths
-			count += len(f.String())
-		case reflect.Ptr:
-			// unroll pointers
-			el := f.Elem()
-			if !el.IsValid() {
-				return 0
-			}
-			count += CheckEmbed(el)
-		case reflect.Struct:
-			// recurse over struct fields
-			count += CheckEmbed(f.Interface())
-		default:
-			continue
+	// type assert e
+	ev, ok := e.(reflect.Value)
+	if !ok {
+		ev = reflect.ValueOf(e)
+	}
+
+	switch ev.Kind() {
+	case reflect.String:
+		// count string lengths
+		count += len(ev.String())
+
+	case reflect.Ptr:
+		// unroll pointers
+		elv := ev.Elem()
+		if !elv.IsValid() {
+			return 0
 		}
+		count += Strlen(elv)
+
+	case reflect.Struct:
+		// iterate over fields
+		for i := 0; i < ev.NumField(); i++ {
+			switch f := ev.Field(i); f.Kind() {
+			case reflect.String:
+				// count string field lengths
+				count += len(f.String())
+
+			case reflect.Ptr:
+				// unroll pointers
+				el := f.Elem()
+				if !el.IsValid() {
+					return 0
+				}
+				count += Strlen(el)
+
+			case reflect.Struct:
+				// recurse over struct fields
+				count += Strlen(f.Interface())
+
+			default:
+				// do nothing
+			}
+		}
+	default:
+		// do nothing
 	}
 	return count
 }
