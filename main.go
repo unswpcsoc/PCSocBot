ba409ce2aed92de24714de390db962b6980394eb
VineBalloon <vincentchen2.033@gmail.com>
2019-05-10 18:16:53 +1000

    did more things

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"log"
@@ -14,35 +15,39 @@ import (
 )
 
 const (
-	MESSAGE_LIMIT = 2000
+	MESSAGE_LIMIT = 2000 // discord's message character limit
 )
 
 var (
 	_testing = false
 
-	echo   bool
+	echo    bool
+	verbose bool
+
 	dg     *discordgo.Session
 	router Router
 	prefix = "!"
+
+	errs = log.New(os.Stderr)
 )
 
-// leaf is a leaf of the command router tree.
-type leaf struct {
+// Leaf is a leaf of the command router tree.
+type Leaf struct {
 	command com.Command
-	leaves  map[string]*leaf
+	leaves  map[string]*Leaf
 }
 
-// NewLeaf returns a leaf with the command.
+// NewLeaf returns a Leaf with the command.
 func NewLeaf(cm com.Command) *leaf {
-	return &leaf{
+	return &Leaf{
 		command: cm,
-		leaves:  make(map[string]*leaf),
+		leaves:  make(map[string]*Leaf),
 	}
 }
 
 // Router routes a command string to a command.
 type Router struct {
-	routes *leaf
+	routes *Leaf
 }
 
 // NewRouter returns a new Router structure.
@@ -51,9 +56,14 @@ func NewRouter() Router {
 }
 
 // AddCommand adds command-string mapping
+// e.g.
+//     // c is of type Command
+//     AddCommand(c, []string{"ask"})
+//     AddCommand(c, []string{"tags", "ping"})
+//
+// Will register the commeand c to the command strings "ask" and "tags ping".
+// Calling Route with these command strings will return the command mapped to it.
 func (r *Router) AddCommand(cm com.Command, names []string) {
-	// AddCommand(PingTagPeople, {"ask"})
-	// AddCommand(PingTagPeople, {"tags", "ping"})
 	if cm == nil || len(names) == 0 || r.routes == nil {
 		return
 	}
@@ -90,8 +100,8 @@ func (r *Router) AddCommand(cm com.Command, names []string) {
 //	   // r has a route through "example"->"command"->"string"
 //     com, ind := r.Route([]string{"example", "command", "string", "with", "args"})
 //
-//	   // com will contain the command at "string" leaf
-//	   // ind will be 3
+// `com` will contain the command at "string" leaf
+// `ind` will be 3
 func (r *Router) Route(argv []string) (com.Command, int) {
 	if r.routes == nil || len(argv) == 0 {
 		return nil, 0
@@ -100,7 +110,7 @@ func (r *Router) Route(argv []string) (com.Command, int) {
 	// iterate through routes
 	i := 0
 	curr := r.routes
-	var prev *leaf = nil
+	var prev *Leaf = nil
 	var ok bool
 	for i = 0; i < len(argv); i++ {
 		curr, ok = curr.leaves[argv[i]]
@@ -108,6 +118,10 @@ func (r *Router) Route(argv []string) (com.Command, int) {
 			break
 		}
 		prev = curr
+	}
+
+	if prev == nil {
+		return nil, i
 	}
 
 	return prev.command, i
@@ -119,31 +133,31 @@ func init() {
 		return
 	}
 
-	flag.BoolVar(&echo, "echo", false, "Set this flag to enable echo mode")
+	flag.BoolVar(&echo, "echo", false, "Enables echo mode")
+	flag.BoolVar(&verbose, "verbose", false, "Enables verbose mode")
 	flag.Parse()
 }
 
 // discordgo things
 func init() {
-	log.Println("testing is:", _testing)
 	if _testing {
 		return
 	}
 
-	token, exists := os.LookupEnv("TOKEN")
+	key, exists := os.LookupEnv("KEY")
 	if !exists {
-		log.Fatalln("Missing Discord API Key: Set env var $TOKEN")
+		errs.Fatalln("Missing Discord API Key: Set env var $KEY")
 	}
 
 	var err error
-	dg, err = discordgo.New("Bot " + token)
+	dg, err = discordgo.New("Bot " + key)
 	if err != nil {
-		log.Fatalln("Error:", err)
+		errs.Fatalln("Error:", err)
 	}
 
 	err = dg.Open()
 	if err != nil {
-		log.Fatalln("Error: ", err)
+		errs.Fatalln("Error: ", err)
 	}
 }
 
@@ -161,71 +175,96 @@ func init() {
 
 func main() {
 	defer dg.Close()
-	dg.UpdateListeningStatus("you")
 
 	if echo {
-		fmt.Print("Enter a channel ID: ")
+		// echo mode
+		dg.UpdateListeningStatus("stdin")
 
+		fmt.Print("Enter a channel ID: ")
 		var outch string
-		if got, _ := fmt.Scanf("%s", &outch); got == 0 {
-			log.Fatalln("Try again")
+		if _, err := fmt.Scanf("%s", &outch); err != nil {
+			errs.Fatalln(err)
 		}
 
 		if _, err := dg.Channel(outch); err != nil {
-			log.Fatalln(err)
+			errs.Fatalln(err)
 		}
 
-		var outstr string
+		outstr := ""
+		reader := bufio.NewReader(os.Stdin)
 		fmt.Print("\n>")
-		got, _ := fmt.Scanln(&outstr)
-		for got != 0 {
+
+		got, err := reader.ReadString('\n')
+		if verbose {
+			log.Println("Got string:", got)
+		}
+
+		for err == nil {
 			if len(outstr) > MESSAGE_LIMIT {
 				fmt.Println("Error: Message above message limit")
 				continue
 			}
-			dg.ChannelMessageSend(outch, outstr)
+
+			msg, _ := dg.ChannelMessageSend(outch, got)
+			if verbose {
+				log.Println("Send message:", msg)
+			}
+
 			fmt.Print("\n>")
-			got, _ = fmt.Scanln(&outstr)
+			got, err = reader.ReadString('\n')
+			if verbose {
+				log.Println("Got string:", got)
+			}
 		}
-	} else {
-		dg.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
-			if m.Author.ID == s.State.User.ID {
-				return
-			}
 
-			message := strings.TrimSpace(m.Content)
-
-			if !strings.HasPrefix(message, prefix) {
-				return
-			}
-
-			s.ChannelTyping(m.ChannelID)
-
-			command, ind := router.Route(strings.Split(message, " "))
-			if ind == 0 {
-				// TODO: print help message
-				s.ChannelMessageSend(m.ChannelID, "Error: Unknown command")
-			}
-
-			// Call handler
-			send, err := command.MsgHandle(s, m.Message)
-			if err != nil {
-				log.Println(err)
-				s.ChannelMessageSend(m.ChannelID, "Error: "+err.Error())
-				return
-			}
-
-			// SEND IT
-			send.Send(s)
-		})
-
-		// Don't close the connection, wait for a kill signal
-		log.Println("Logged in as: ", dg.State.User.ID)
-		sc := make(chan os.Signal, 1)
-		signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
-		sig := <-sc
-		log.Println("Received Signal: " + sig.String())
+		return
 	}
+	dg.UpdateListeningStatus("you")
 
+	dg.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
+		if m.Author.ID == s.State.User.ID {
+			return
+		}
+
+		message := strings.TrimSpace(m.Content)
+		if !strings.HasPrefix(message, prefix) || len(message) == 1 {
+			return
+		}
+
+		s.ChannelTyping(m.ChannelID)
+
+		cmdstr := message[1:]
+		command, _ := router.Route(strings.Split(cmdstr, " "))
+		if verbose {
+			log.Println("Routed string", cmdstr, "to command", command)
+		}
+		if command == nil {
+			// TODO help message routing
+			s.ChannelMessageSend(m.ChannelID, "Error: Unknown command")
+			return
+		}
+
+		// Call handler
+		send, err := command.MsgHandle(s, m.Message)
+		if verbose {
+			log.Println("Handled message", m.Message, "\nHandler returned", send)
+		}
+		if err != nil {
+			errs.Println(err)
+			s.ChannelMessageSend(m.ChannelID, "Error: "+err.Error())
+			return
+		}
+
+		// SEND IT
+		send.Send(s)
+	})
+
+	// Don't close the connection, wait for a kill signal
+	log.Println("Logged in as:", dg.State.User.ID)
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	sig := <-sc
+
+	log.Println("Received Signal: " + sig.String())
 	log.Println("Bye!")
 }
