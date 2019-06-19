33b42fb91d585cd02eea9708895f26cc9db845b5
thesabinelim <thesabinelim@gmail.com>
2019-06-19 14:04:06 +1000

    Improved style

--- commands/decimal_spiral.go
+++ commands/decimal_spiral.go
@@ -4,14 +4,14 @@ import (
 	"errors"
 	"strconv"
 
-    "github.com/bwmarrin/discordgo"
+	"github.com/bwmarrin/discordgo"
 
-    "github.com/unswpcsoc/PCSocBot/utils"
+	"github.com/unswpcsoc/PCSocBot/utils"
 )
 
 const(
-    DecimalSpiralLowerLimit = 5
-    DecimalSpiralUpperLimit = 43
+	DecimalSpiralLowerLimit = 5
+	DecimalSpiralUpperLimit = 43
 )
 
 var (
@@ -53,33 +53,28 @@ func (d *DecimalSpiral) MsgHandle(ses *discordgo.Session, msg *discordgo.Message
 		return nil, ErrDecimalSpiralArgs
 	}
 
-    size, err := strconv.Atoi(args[0]);
-    if err != nil {
-        if err.(*strconv.NumError).Err == strconv.ErrSyntax {
-            return nil, ErrDecimalSpiralSyntax
-        } else if err.(*strconv.NumError).Err == strconv.ErrRange {
-            return nil, ErrDecimalSpiralRange
-        } else {
-            return nil, err
-        }
-    }
+	size, err := strconv.Atoi(args[0]);
+	if err != nil {
+		if err.(*strconv.NumError).Err == strconv.ErrSyntax {
+			return nil, ErrDecimalSpiralSyntax
+		} else if err.(*strconv.NumError).Err == strconv.ErrRange {
+			return nil, ErrDecimalSpiralRange
+		}
+		return nil, err
+	}
 
-	if isEven(size) || size < DecimalSpiralLowerLimit || size > DecimalSpiralUpperLimit {
+	if size % 2 == 0 || size < DecimalSpiralLowerLimit || size > DecimalSpiralUpperLimit {
 		return nil, ErrDecimalSpiralRange
 	}
 
 	return NewSimpleSend(msg.ChannelID, utils.Block(genDecimalSpiral(size))), nil
 }
 
-func isEven(i int) bool {
-	return i % 2 == 0
-}
-
 func abs(i int) int {
-    if (i < 0) {
-        return -i
-    }
-    return i
+	if (i < 0) {
+		return -i
+	}
+	return i
 }
 
 // genDecimalSpiral consists of 2 steps:
@@ -91,59 +86,59 @@ func abs(i int) int {
 func genDecimalSpiral(size int) string {
 	out := ""
 	row := 0
-    for row < size {
-        col := 0
-        for col < size {
-            if isDigit(size, row, col) {
+	for row < size {
+		col := 0
+		for col < size {
+			if isDigit(size, row, col) {
 				out += strconv.Itoa(getDigit(size, row, col) % 10)
-            } else {
+			} else {
 				out += "-"
-            }
-            col++
-        }
-        out += "\n"
-        row++
-    }
+			}
+			col++
+		}
+		out += "\n"
+		row++
+	}
 
-    return out
+	return out
 }
 
 // isDigit determines whether a coordinate on a square is part of
 // the spiral of digits and returns true if it is, and false if not
 func isDigit(size int, row int, col int) bool {
-    // Absolute row distance from midpoint
-    rowDist := abs(row - size / 2)
-    // Absolute column distance from midpoint
-    colDist := abs(col - size / 2)
+	// Absolute row distance from midpoint
+	rowDist := abs(row - size / 2)
+	// Absolute column distance from midpoint
+	colDist := abs(col - size / 2)
 	isDigit := false
 
-    if size % 4 == 1 {
-        // Type 1 spiral (digit in centre)
-        if row <= size / 2 && col < size / 2 && row == col + 1 {
-            // Special handling to turn boxes into spirals
-            if rowDist % 2 == 0 {
-                isDigit = true
-            }
-        } else if colDist >= rowDist && isEven(colDist) { 
-            isDigit = true
-        } else if colDist < rowDist && isEven(rowDist) {
-            isDigit = true
-        }
-    } else {
-        // Type 2 spiral (no digit in centre)
-        if row <= size / 2 && col < size / 2 && row == col + 1 {
-            // Special handling to turn boxes into spirals
-            if rowDist % 2 == 1 {
-                isDigit = true
-            }
-        } else if colDist >= rowDist && !isEven(colDist) {
-            isDigit = true
-        } else if colDist < rowDist && !isEven(rowDist) {
-            isDigit = true
-        }
+	if size % 4 == 1 {
+		// Type 1 spiral (digit in centre)
+		if row <= size / 2 && col < size / 2 && row == col + 1 {
+			// Special handling to turn boxes into spirals
+			if rowDist % 2 == 0 {
+				isDigit = true
+			}
+		} else if colDist >= rowDist && colDist % 2 == 0 { 
+			isDigit = true
+		} else if colDist < rowDist && rowDist % 2 == 0 {
+			isDigit = true
+		}
+	} else {
+		// Type 2 spiral (no digit in centre)
+		if row <= size / 2 && col < size / 2 && row == col + 1 {
+			// Special handling to turn boxes into spirals
+			if rowDist % 2 == 1 {
+				isDigit = true
+			}
+		} else if colDist >= rowDist && colDist % 2 != 0 {
+			isDigit = true
+		} else if colDist < rowDist && rowDist % 2 != 0 {
+			isDigit = true
+		}
 	}
 
-    return isDigit
+	return isDigit
 }
 
 // getDigit calculates the digit to print at a coordinate of a
@@ -179,7 +174,7 @@ func isDigit(size int, row int, col int) bool {
 // quadratic sequence
 // 6  30  70  126
 //  24  40  56
-//    16  16
+//	16  16
 // Where the differences between the differences is 16.
 // Using this, you can make a quadratic equation for the top left
 // corners of each box, and subtract the current column to find the
@@ -198,61 +193,61 @@ func getDigit(size int, row int, col int) int {
 		col = col - 1
 	}
 
-    // Row displacement from midpoint
-    rowDist := row - size / 2
-    // Absolute row distance from midpoint
-    absRowDist := abs(rowDist)
-    // Column displacement from midpoint
-    colDist := col - size / 2
-    // Absolute column distance from midpoint
-    absColDist := abs(colDist)
-    // Size of box current coordinate is on
-    subSize := 0
-    if absRowDist >= absColDist {
-        subSize = 2 * absRowDist + 1
-    } else {
-        subSize = 2 * absColDist + 1
-    }
-    row = row - (size - subSize) / 2
-    col = col - (size - subSize) / 2
-    // Layer of current box. 0 is centre
+	// Row displacement from midpoint
+	rowDist := row - size / 2
+	// Absolute row distance from midpoint
+	absRowDist := abs(rowDist)
+	// Column displacement from midpoint
+	colDist := col - size / 2
+	// Absolute column distance from midpoint
+	absColDist := abs(colDist)
+	// Size of box current coordinate is on
+	subSize := 0
+	if absRowDist >= absColDist {
+		subSize = 2 * absRowDist + 1
+	} else {
+		subSize = 2 * absColDist + 1
+	}
+	row = row - (size - subSize) / 2
+	col = col - (size - subSize) / 2
+	// Layer of current box. 0 is centre
 	layer := (subSize + 1) / 4
 
-    if rowDist <= 0 && absRowDist >= absColDist {
-        // Top quadrant
-        if subSize % 4 == 1 {
-            // Type 1 boxes
-            return 8 * layer * layer + 8 * layer - col
-        } else {
-            // Type 2 boxes
-            return 8 * layer * layer - 2 - col
-        }
-    } else if colDist > 0 && absColDist > absRowDist {
-        // Right quadrant
-        if subSize % 4 == 1 {
-            // Type 1 boxes
-            return 8 * layer * layer + 4 * layer - row
-        } else {
-            // Type 2 boxes
-            return 8 * layer * layer - 4 * layer - row
-        }
-    } else if rowDist > 0 && absRowDist >= absColDist {
-        // Bottom quadrant
-        if subSize % 4 == 1 {
-            // Type 1 boxes
-            return 8 * layer * layer - 4 * layer + col
-        } else {
-            // Type 2 boxes
-            return 8 * layer * layer - 12 * layer + 4 + col
-        }
-    } else {
-        // Left quadrant
-        if subSize % 4 == 1 {
-            // Type 1 boxes
-            return 8 * layer * layer - 8 * layer + row
-        } else {
-            // Type 2 boxes
-            return 8 * layer * layer - 16 * layer + 6 + row
-        }
-    }
+	if rowDist <= 0 && absRowDist >= absColDist {
+		// Top quadrant
+		if subSize % 4 == 1 {
+			// Type 1 boxes
+			return 8 * layer * layer + 8 * layer - col
+		} else {
+			// Type 2 boxes
+			return 8 * layer * layer - 2 - col
+		}
+	} else if colDist > 0 && absColDist > absRowDist {
+		// Right quadrant
+		if subSize % 4 == 1 {
+			// Type 1 boxes
+			return 8 * layer * layer + 4 * layer - row
+		} else {
+			// Type 2 boxes
+			return 8 * layer * layer - 4 * layer - row
+		}
+	} else if rowDist > 0 && absRowDist >= absColDist {
+		// Bottom quadrant
+		if subSize % 4 == 1 {
+			// Type 1 boxes
+			return 8 * layer * layer - 4 * layer + col
+		} else {
+			// Type 2 boxes
+			return 8 * layer * layer - 12 * layer + 4 + col
+		}
+	} else {
+		// Left quadrant
+		if subSize % 4 == 1 {
+			// Type 1 boxes
+			return 8 * layer * layer - 8 * layer + row
+		} else {
+			// Type 2 boxes
+			return 8 * layer * layer - 16 * layer + 6 + row
+		}
+	}
 }
